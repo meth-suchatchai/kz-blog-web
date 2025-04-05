3cf2f0681c01f23b5095e9b75b76c764e8e10925
KuroshibaZ <lazanya_70@hotmail.com>
2025-04-05 18:01:02 +0700

    [workflow] change directory desitnation path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    output: "export"
+    output: "export",
+    distDir: "kuroshibaz.dev",
+    images: {
+        unoptimized: true,
+    }
 };
 
 export default nextConfig;
